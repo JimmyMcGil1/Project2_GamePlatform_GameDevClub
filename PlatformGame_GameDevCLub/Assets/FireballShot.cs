82fbbb112ba8cba57090c3b439d3b2a7fd352750
PhatVND <phat.vnduc@gmail.com>
2023-04-13 17:38:55 +0700

    Animation Changed

--- PlatformGame_GameDevCLub/Assets/FireballShot.cs
+++ PlatformGame_GameDevCLub/Assets/FireballShot.cs
@@ -19,7 +19,7 @@ public class FireballShot : MonoBehaviour
         rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
 
         float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+        transform.rotation = Quaternion.Euler(0, 0, rot);
     }
 
     // Update is called once per frame
